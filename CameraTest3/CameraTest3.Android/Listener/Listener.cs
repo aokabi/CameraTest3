d169d8d063320ac662548ae0b7e9b506ba78a619
aokabi <aokabit@gmail.com>
2018-02-20 02:59:42 +0900

    プロジェクト ファイルを追加します。

---
+++ CameraTest3/CameraTest3.Android/Listener/Listener.cs
@@ -0,0 +1,101 @@
+﻿using Android.Graphics;
+using Android.Hardware.Camera2;
+using Android.Runtime;
+using Android.Views;
+using System;
+
+namespace CameraTest3.Droid
+{
+    public class CameraStateListener : CameraDevice.StateCallback
+    {
+        private readonly MainActivity owner;
+
+        public CameraStateListener(MainActivity owner)
+        {
+            this.owner = owner ?? throw new ArgumentNullException("owner");
+        }
+
+        public override void OnDisconnected(CameraDevice camera)
+        {
+            camera.Close();
+            owner.mCameraDevice = null;
+        }
+
+        public override void OnError(CameraDevice camera, [GeneratedEnum] CameraError error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnOpened(CameraDevice camera)
+        {
+            owner.mCameraDevice = camera;
+
+            owner.mCaptureRequestBuilder = owner.mCameraDevice.CreateCaptureRequest(CameraTemplate.Preview);
+            var surface = new Surface(owner.mSurfaceTexture);
+            owner.mSurfaces.Add(surface);
+            owner.mCaptureRequestBuilder.AddTarget(surface);
+
+            owner.mCameraDevice.CreateCaptureSession(owner.mSurfaces, owner.mCaptureSessionCallBack, null);
+        }
+    }
+
+    public class CameraCaptureSessionCallback : CameraCaptureSession.StateCallback
+    {
+        private readonly MainActivity owner;
+
+        public CameraCaptureSessionCallback(MainActivity owner)
+        {
+            this.owner = owner ?? throw new ArgumentNullException("owner");
+        }
+        public override void OnConfigured(CameraCaptureSession session)
+        {// sessionが生成されたとき
+            owner.mCaptureSession = session;
+            owner.mCaptureRequest = owner.mCaptureRequestBuilder.Build();
+            session.SetRepeatingRequest(owner.mCaptureRequest, owner.mCameraCaptureSessionCaptureCallback, null);
+        }
+
+        public override void OnConfigureFailed(CameraCaptureSession session)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CameraCaptureSessionCaptureCallback : CameraCaptureSession.CaptureCallback
+    {
+
+    }
+
+    public class SurfaceTextureListener : Java.Lang.Object, TextureView.ISurfaceTextureListener
+    {
+        private readonly MainActivity owner;
+
+        public SurfaceTextureListener(MainActivity owner)
+        {
+            this.owner = owner ?? throw new ArgumentNullException("owner");
+        }
+
+        public void OnSurfaceTextureAvailable(SurfaceTexture surface, int width, int height)
+        {// SurfaceViewの準備ができたら
+            owner.mSurfaceTexture = surface;
+
+            owner.OpenCamera();
+
+
+        }
+
+        public bool OnSurfaceTextureDestroyed(SurfaceTexture surface)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSurfaceTextureUpdated(SurfaceTexture surface)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ CameraTest3/CameraTest3.Android/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.Hardware.Camera2;
+using Android.OS;
+using Android.Views;
+using System.Collections.Generic;
+
+namespace CameraTest3.Droid
+{
+    [Activity(Label = "CameraTest3", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public CameraStateListener stateCallBack;
+        public CameraCaptureSession mCaptureSession;
+        public CameraCaptureSessionCallback mCaptureSessionCallBack;
+        public CameraCaptureSessionCaptureCallback mCameraCaptureSessionCaptureCallback;
+        public CaptureRequest.Builder mCaptureRequestBuilder;
+        public CaptureRequest mCaptureRequest;
+
+        public CameraDevice mCameraDevice;
+
+        public SurfaceTexture mSurfaceTexture;
+
+        public List<Surface> mSurfaces = new List<Surface>();
+
+        private TextureView mTextureView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+            stateCallBack = new CameraStateListener(this);
+
+            mTextureView = new TextureView(this);
+            mSurfaceTexture = mTextureView.SurfaceTexture;
+
+            this.SetContentView(mTextureView);
+
+
+            mTextureView.SurfaceTextureListener = new SurfaceTextureListener(this);
+            mCaptureSessionCallBack = new CameraCaptureSessionCallback(this);
+            
+
+
+        }
+
+        public void OpenCamera()
+        {
+            var manager = (CameraManager)this.GetSystemService(Context.CameraService);
+            foreach (var cameraId in manager.GetCameraIdList())
+            {
+                CameraCharacteristics characteristics = manager.GetCameraCharacteristics(cameraId);
+                if ((int)characteristics.Get(CameraCharacteristics.LensFacing) == (int)LensFacing.Back)
+                {
+                    manager.OpenCamera(cameraId, stateCallBack, null);
+                    return;
+                }
+            }
+        }
+    }
+
+    
+}
+
